1411b29a551b592a59480176e887657372719433
ahmedberriche <ahmedberriche1993@gmail.com>
2022-11-14 16:03:30 +0100

    fix banner style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,10 +10,11 @@ import MainLayout from "../components/MainLayout";
 import Banner from "../components/Banner";
 import Player from "../components/Player";
 import SectionTitle from "../components/SectionTitle";
+import { cardsDataSet } from "../utils";
 
 const RootStyle = styled("div")({
   background: "rgb(249, 250, 251)",
-  height: "calc(100% - 64px)",
+  height: "100%",
   placeItems: "center",
   display: "flex",
 });
@@ -22,7 +23,6 @@ const ContentStyle = styled("div")({
   maxWidth: "90%",
   width: "90%",
   height: "100%",
-  padding: 25,
   margin: "auto",
   background: "#fff",
 });
@@ -43,91 +43,24 @@ const fadeInUp = {
     },
   },
 };
+const translateY = {
+  initial: {
+    x: -1000,
+    transition: { duration: 0.6, ease: easing },
+  },
+  animate: {
+    x: 0,
+    transition: {
+      duration: 0.8,
+      ease: easing,
+    },
+  },
+};
 const CardStyle = styled(Box)({
   textAlign: "center",
 });
 
 const Home = ({ _ }) => {
-  const dataSet = [
-    {
-      title: "RH",
-      subTitle: "",
-      image: "assets/images/numeryx-univ.jpg",
-      cardData: [
-        {
-          iconSrc: "CRA.jpg",
-          title: "CRA",
-          uri: "https://ui.boondmanager.com/dashboard",
-        },
-        {
-          iconSrc: "DocAdm.png",
-          title: "document administratifs",
-          uri: "https://apps.powerapps.com/play/e/default-f050ee11-8c72-4be6-80ce-5238fb70166a/a/1f6aa88e-2697-4767-a43e-75f98b96f2b2?source=teamsopenwebsite&screenColor=rgba(0,+176,+240,+1)&hint=0492cae0-3b41-4a01-811f-e1ff760ac293",
-        },
-        {
-          iconSrc: "coffre-fort.png",
-          title: "Coffre-fort numérique",
-          uri: "https://edocperso.fr/login",
-        },
-      ],
-    },
-    {
-      title: "Numeryx université",
-      subTitle: "",
-      image: "assets/images/numeryx-univ.jpg",
-      cardData: [
-        {
-          iconSrc: "LMS.png",
-          title: "LMS",
-          uri: "https://tech.numeryx.fr:6500/moodle/",
-        },
-        {
-          iconSrc: "catalogue-formation.png",
-          title: "Catalogue de formation",
-          uri: "https://university.numeryx.fr/fr/nos-formations-professionnelles",
-        },
-        {
-          iconSrc: "demande-formation.jpg",
-          title: "Demande de formation",
-          uri: "https://apps.powerapps.com/play/e/default-f050ee11-8c72-4be6-80ce-5238fb70166a/a/c03cacde-4399-4440-934b-d4808dd04de5?tenantId=f050ee11-8c72-4be6-80ce-5238fb70166a",
-        },
-      ],
-    },
-    {
-      title: "Demandes et réclamations",
-      subTitle: "",
-      image: "assets/images/numeryx-univ.jpg",
-      cardData: [
-        {
-          iconSrc: "problem-claim.png",
-          title: "Porblem Claim",
-          uri: "https://apps.powerapps.com/play/0cc9e371-c2fd-4054-bccc-3c11017bd9f4?tenantId=f050ee11-8c72-4be6-80ce-5238fb70166a",
-        },
-        {
-          iconSrc: "GLPI.png",
-          title: "GLPI",
-          uri: "http://support.numeryx.fr/Glpi-Numeryx/index.php?redirect=%2Ffront%2Ftracking.injector.php&error=3",
-        },
-      ],
-    },
-    {
-      title: "Réseaux sociaux ",
-      subTitle: "",
-      image: "assets/images/numeryx-univ.jpg",
-      cardData: [
-        {
-          iconSrc: "yammer.jpg",
-          title: "Yammer",
-          uri: "https://www.yammer.com/login",
-        },
-        {
-          iconSrc: "linkedin.png",
-          title: "LINKEDIN",
-          uri: "https://www.linkedin.com/search/results/all/?heroEntityKey=urn%3Ali%3Aorganization%3A18574742&keywords=numeryx&origin=RICH_QUERY_TYPEAHEAD_HISTORY&position=0&searchId=6e3e7c2a-d776-402e-8da1-070d3a255a7c&sid=5_i",
-        },
-      ],
-    },
-  ];
   const playerData = [
     { src: "http://techslides.com/demos/sample-videos/small.ogv" },
     { src: "http://techslides.com/demos/sample-videos/small.ogv" },
@@ -140,11 +73,11 @@ const Home = ({ _ }) => {
         <Header />
         <RootStyle>
           <ContentStyle>
-            <Banner {...fadeInUp} />
+            <Banner {...translateY} />
             <SectionTitle title="Section 1 " />
             <CardStyle component={motion.div} {...fadeInUp}>
               <div className="card-container">
-                {dataSet.map((item, index) => (
+                {cardsDataSet.map((item, index) => (
                   <div key={index} className="card-content">
                     <Card {...item} />
                   </div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,84 @@ export const isMobileDevice =
   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
     navigator.userAgent
   );
+
+export const cardsDataSet = [
+  {
+    title: "RH",
+    subTitle: "",
+    image: "assets/images/numeryx-univ.jpg",
+    cardData: [
+      {
+        iconSrc: "CRA.jpg",
+        title: "CRA",
+        uri: "https://ui.boondmanager.com/dashboard",
+      },
+      {
+        iconSrc: "DocAdm.png",
+        title: "document administratifs",
+        uri: "https://apps.powerapps.com/play/e/default-f050ee11-8c72-4be6-80ce-5238fb70166a/a/1f6aa88e-2697-4767-a43e-75f98b96f2b2?source=teamsopenwebsite&screenColor=rgba(0,+176,+240,+1)&hint=0492cae0-3b41-4a01-811f-e1ff760ac293",
+      },
+      {
+        iconSrc: "coffre-fort.png",
+        title: "Coffre-fort numérique",
+        uri: "https://edocperso.fr/login",
+      },
+    ],
+  },
+  {
+    title: "Numeryx université",
+    subTitle: "",
+    image: "assets/images/numeryx-univ.jpg",
+    cardData: [
+      {
+        iconSrc: "LMS.png",
+        title: "LMS",
+        uri: "https://tech.numeryx.fr:6500/moodle/",
+      },
+      {
+        iconSrc: "catalogue-formation.png",
+        title: "Catalogue de formation",
+        uri: "https://university.numeryx.fr/fr/nos-formations-professionnelles",
+      },
+      {
+        iconSrc: "demande-formation.jpg",
+        title: "Demande de formation",
+        uri: "https://apps.powerapps.com/play/e/default-f050ee11-8c72-4be6-80ce-5238fb70166a/a/c03cacde-4399-4440-934b-d4808dd04de5?tenantId=f050ee11-8c72-4be6-80ce-5238fb70166a",
+      },
+    ],
+  },
+  {
+    title: "Demandes et réclamations",
+    subTitle: "",
+    image: "assets/images/numeryx-univ.jpg",
+    cardData: [
+      {
+        iconSrc: "problem-claim.png",
+        title: "Porblem Claim",
+        uri: "https://apps.powerapps.com/play/0cc9e371-c2fd-4054-bccc-3c11017bd9f4?tenantId=f050ee11-8c72-4be6-80ce-5238fb70166a",
+      },
+      {
+        iconSrc: "GLPI.png",
+        title: "GLPI",
+        uri: "http://support.numeryx.fr/Glpi-Numeryx/index.php?redirect=%2Ffront%2Ftracking.injector.php&error=3",
+      },
+    ],
+  },
+  {
+    title: "Réseaux sociaux ",
+    subTitle: "",
+    image: "assets/images/numeryx-univ.jpg",
+    cardData: [
+      {
+        iconSrc: "yammer.jpg",
+        title: "Yammer",
+        uri: "https://www.yammer.com/login",
+      },
+      {
+        iconSrc: "linkedin.png",
+        title: "LINKEDIN",
+        uri: "https://www.linkedin.com/search/results/all/?heroEntityKey=urn%3Ali%3Aorganization%3A18574742&keywords=numeryx&origin=RICH_QUERY_TYPEAHEAD_HISTORY&position=0&searchId=6e3e7c2a-d776-402e-8da1-070d3a255a7c&sid=5_i",
+      },
+    ],
+  },
+];
